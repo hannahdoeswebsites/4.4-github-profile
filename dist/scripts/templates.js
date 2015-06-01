406078c31f3b6b002cc58ca2e5c19cb107cb22ce
Hannah Jordan <hjorda0126@aol.com>
2015-06-01 00:19:44 -0400

    Most styling complete. All links working, including followers/following. Still need to add top bar tomorrow afternoon.

--- dist/scripts/templates.js
+++ dist/scripts/templates.js
@@ -5,17 +5,23 @@ this["JST"]["application"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1
 this["JST"]["repos"] = Handlebars.template({"1":function(depth0,helpers,partials,data) {
     var helper, alias1=helpers.helperMissing, alias2="function", alias3=this.escapeExpression;
 
-  return "  <div id=\"name\">"
+  return "  <div id=\"name\"><a href=\""
+    + alias3(((helper = (helper = helpers.url || (depth0 != null ? depth0.url : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"url","hash":{},"data":data}) : helper)))
+    + "\">"
     + alias3(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"name","hash":{},"data":data}) : helper)))
-    + "</div>\n  <div id=\"language\">"
+    + "<a></div>\n  <div id=\"language\">"
     + alias3(((helper = (helper = helpers.language || (depth0 != null ? depth0.language : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"language","hash":{},"data":data}) : helper)))
-    + "</div> <div id=\"stargazers\"><span class=\"octicon octicon-star\"></span> "
+    + "</div>\n  <div id=\"stargazers\"><a href=\""
+    + alias3(((helper = (helper = helpers.stargazers_url || (depth0 != null ? depth0.stargazers_url : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"stargazers_url","hash":{},"data":data}) : helper)))
+    + "\"><span class=\"octicon octicon-star\"></span> "
     + alias3(((helper = (helper = helpers.stargazers_count || (depth0 != null ? depth0.stargazers_count : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"stargazers_count","hash":{},"data":data}) : helper)))
-    + "</div> <div id=\"forks\"><span class=\"octicon octicon-git-branch\"></span> "
+    + "</a></div>\n  <div id=\"forks\"><a href=\""
+    + alias3(((helper = (helper = helpers.forks_url || (depth0 != null ? depth0.forks_url : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"forks_url","hash":{},"data":data}) : helper)))
+    + "\"><span class=\"octicon octicon-git-branch\"></span> "
     + alias3(((helper = (helper = helpers.forks_count || (depth0 != null ? depth0.forks_count : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"forks_count","hash":{},"data":data}) : helper)))
-    + "</div>\n  <div id=\"update\">"
+    + "</a></div>\n  <div id=\"update\"> Updated "
     + alias3(((helper = (helper = helpers.updated_at || (depth0 != null ? depth0.updated_at : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"updated_at","hash":{},"data":data}) : helper)))
-    + "</div>\n\n";
+    + " days ago.</div>\n\n";
 },"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
     var stack1;
 
@@ -35,5 +41,15 @@ this["JST"]["users"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"ma
     + alias3(((helper = (helper = helpers.location || (depth0 != null ? depth0.location : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"location","hash":{},"data":data}) : helper)))
     + "</p>\n<p id=\"datejoined\"> <span class=\"octicon octicon-clock\"></span> Joined On "
     + alias3(((helper = (helper = helpers.created_at || (depth0 != null ? depth0.created_at : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"created_at","hash":{},"data":data}) : helper)))
-    + "</p>\n";
+    + "</p>\n\n<div class=\"numbers\">\n<p id=\"followers\"><a href=\""
+    + alias3(((helper = (helper = helpers.followers_url || (depth0 != null ? depth0.followers_url : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"followers_url","hash":{},"data":data}) : helper)))
+    + "\"> "
+    + alias3(((helper = (helper = helpers.followers || (depth0 != null ? depth0.followers : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"followers","hash":{},"data":data}) : helper)))
+    + " </a> Followers </p><p id=\"starred\"> <a href=\""
+    + alias3(((helper = (helper = helpers.starred_url || (depth0 != null ? depth0.starred_url : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"starred_url","hash":{},"data":data}) : helper)))
+    + "\"> 0 </a>Starred</p><p id=\"following\"><a href=\""
+    + alias3(((helper = (helper = helpers.following_url || (depth0 != null ? depth0.following_url : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"following_url","hash":{},"data":data}) : helper)))
+    + "\"> "
+    + alias3(((helper = (helper = helpers.following || (depth0 != null ? depth0.following : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"following","hash":{},"data":data}) : helper)))
+    + "</a> Following</p>\n</div>\n";
 },"useData":true});
